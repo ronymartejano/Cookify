ee11d91a00abca368344a1ec7231f0c877e4ef44
nymarr <tejanoronymar@gmail.com>
2025-01-10 09:27:57 +0800

    Remove dotenv configuration from Vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-import dotenv from 'dotenv'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-// Load environment variables from .env file
-dotenv.config()
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
